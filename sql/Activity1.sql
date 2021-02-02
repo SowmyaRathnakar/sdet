d3d0f3290733f16432449fe77c09233582198fba
SowmyaRathnakar <77953867+SowmyaRathnakar@users.noreply.github.com>
2021-02-02 18:20:44 +0530

    Add files via upload

---
+++ sql/Activity1.sql
@@ -0,0 +1,12 @@
+REM   Script: SQLActivity_1
+REM   SQL activity 1 
+
+create table salesman ( 
+SALESMAN_ID int not null,  
+salesman_name varchar2(20), 
+salesman_city varchar2(20), 
+commission int);
+
+desc salesman
+
+
