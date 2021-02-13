5b56b0832054160e2e3c7290f7572f7ea4cdd23e
SowmyaRathnakar <77953867+SowmyaRathnakar@users.noreply.github.com>
2021-02-14 00:39:56 +0530

    Add files via upload

---
+++ sql/Activity5.sql
@@ -0,0 +1,37 @@
+REM   Script: Activity5
+REM   Activity 5 of SQL
+
+create table salesman ( 
+salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+alter table salesman add grade int;
+
+update salesman set grade=100 ;
+
+select * from salesman;
+
+select * from salesman;
+
+update salesman set grade =200 where salesman_city = 'Rome' ;
+
+update salesman set grade= 300 where salesman_name = 'James Hoog';
+
+update salesman set salesman_name = 'Pierre' where salesman_name = 'McLyon';
+
+select * from salesman;
+
